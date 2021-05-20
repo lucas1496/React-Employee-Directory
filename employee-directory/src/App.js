082e56bd24e23f6a095dee862496fc35211a7ad9
Lucas Q <lucas.14.96@gmail.com>
2021-05-20 18:16:06 -0500

    Imported and passed MainContainer component into app

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import './App.css';
 import Header from "./components/Header";
+import MainContainer from "./components/MainContainer";
 
 function App() {
   return (
+    // Created empty tag as "parent", components wouldn't render without it
+    <>
     <Header />
+    <MainContainer />
+    </>
   );
 }
 
