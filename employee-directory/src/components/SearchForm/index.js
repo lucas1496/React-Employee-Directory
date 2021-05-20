280f336ac76dda9124fa9fd8efd67ba8796ac20d
Lucas Q <lucas.14.96@gmail.com>
2021-05-20 16:34:58 -0500

    Modified SearchForm component to fit app

--- employee-directory/src/components/SearchForm/index.js
+++ employee-directory/src/components/SearchForm/index.js
@@ -3,29 +3,23 @@ import "./style.css";
 
 function SearchForm(props) {
     return (
-      <form className="search">
-        <div className="form-group">
-          <label htmlFor="breed">Breed Name:</label>
-          <input
-            value={props.search}
-            onChange={props.handleInputChange}
-            name="breed"
-            list="breeds"
-            type="text"
-            className="form-control"
-            placeholder="Type in a dog breed to begin"
-            id="breed"
-          />
-          <datalist id="breeds">
-            {props.breeds.map(breed => (
-              <option value={breed} key={breed} />
-            ))}
-          </datalist>
-          <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
-            Search
-          </button>
-        </div>
-      </form>
+      <nav>
+        <form className="search">
+          <div className="form-group">
+            <input
+              // value={props.value}
+              // onChange={props.handleInputChange}
+              name="search"
+              type="text"
+              className="form-control"
+              placeholder="Search for an employee"
+            />
+            {/* <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+              Search
+            </button> */}
+          </div>
+        </form>
+      </nav>
     );
 }
   
