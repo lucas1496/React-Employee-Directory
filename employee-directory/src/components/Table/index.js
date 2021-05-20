1f0e4d6db7e7e4d89de13bf0dcf60bc5805493c9
Lucas Q <lucas.14.96@gmail.com>
2021-05-20 17:24:38 -0500

    Created Table component and put react table template into index.js

---
+++ employee-directory/src/components/Table/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Table (props) {
+    return (
+      <table>
+        <caption>Our products</caption>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>In Stock</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(product => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td>{product.stock}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+}
