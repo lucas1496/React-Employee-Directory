c68dc500763b35dec1dbe9cd9c818d3691d15ce5
Lucas Q <lucas.14.96@gmail.com>
2021-05-20 15:33:58 -0500

    Created components folder with Header and SearchForm

---
+++ employee-directory/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => {
+    return (
+        <header>
+          <h1 className="text-center">React Employee Directory</h1>
+          <p className="text-center">
+            Lorem Ipsum Whatever
+          </p>
+        </header>
+      );
+};
+
+export default Header;
